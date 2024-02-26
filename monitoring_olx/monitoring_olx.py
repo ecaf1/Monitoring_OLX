96adad3a46af4227542261226aeb5abadb3b8f01
ecaf1 <edeilsonazevedo22@gmail.com>
2024-02-25 22:22:48 -0300

    Implementa função de pegar dados de anuncio especifico

--- monitoring_olx/monitoring_olx.py
+++ monitoring_olx/monitoring_olx.py
@@ -1,39 +1,60 @@
 import json
 import time
+from math import e
+from urllib.parse import quote
 
 import requests  # cloudscrap
 from bs4 import BeautifulSoup
 
-olx_url = "https://www.olx.com.br/brasil"
-
+OLX_URL = "https://www.olx.com.br/"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}
 novos_anuncios = set()
 intervalo_verificacao = 3600
 
 
-def obter_links_anuncios(url):
-    links = set()
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    anuncios = soup.find_all("a", {"class": "olx-ad-card__link-wrapper"})
-    for anuncio in anuncios:
-        link = anuncio.get("href")
-        if link:
-            links.add(link)
-    print(len(links))
-    return links
+def get_ad_data(produvt, stare_key="brasil/"):
+    my_ads = []
+
+    if stare_key != "brasil/":
+        url = OLX_URL + f"estado-{stare_key.lower()}"
+    else:
+        url = OLX_URL + stare_key
+
+    params = {"q": produvt}
+    try:
+        response = requests.get(url, params=params, headers=HEADERS)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+        ads_gloss = soup.find(id="__NEXT_DATA__")
+        ad_json = json.loads(getattr(ads_gloss, "text"))
+        ads = ad_json["props"]["pageProps"]["ads"]
+        for ad in ads:
+            ad_data = {
+                "title": ad["title"],
+                "price": ad["price"],
+                "url_ad": ad["url"],
+                "url_images": [image["original"] for image in ad["images"]],
+            }
+            my_ads.append(ad_data)
+    except requests.HTTPError as e:
+        print(e)
+    return my_ads
 
 
 def verificar_novos_anuncios():
-    global novos_anuncios
-    print("Verificando novos anuncio...")
-    links_anuncios = obter_links_anuncios(olx_url)
-    novos = links_anuncios - novos_anuncios
-    if novos:
-        for link in novos:
-            print(f"Novos anúncios encontrado: {link}")
-        novos_anuncios.update(novos)
-    else:
-        print("Nenhum novo anuncio encontrado.")
+    ...
+#     global novos_anuncios
+#     print("Verificando novos anuncio...")
+#     links_anuncios = obter_links_anuncios(OLX_URL)
+#     novos = links_anuncios - novos_anuncios
+#     if novos:
+#         for link in novos:
+#             print(f"Novos anúncios encontrado: {link}")
+#         novos_anuncios.update(novos)
+#     else:
+#         print("Nenhum novo anuncio encontrado.")
 
 
 def main():
@@ -45,17 +66,18 @@ def main():
 if __name__ == "__main__":
     # print(novos_anuncios)
     # main()
-    sessao = requests.Session()
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-    }
-    payload = {"q": "caloi%20estradacle"}
-    response = sessao.get(olx_url, headers=headers)
-    print(response.request.headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-    resu = soup.find(id="__NEXT_DATA__")
-    with open("resu.html", "w", encoding="utf-8") as f:
-        f.write(soup.prettify())
-    data = json.loads(getattr(resu, "text"))
-    with open("resu.json", "w") as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    # sessao = requests.Session()
+    # headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    # }
+    # payload = {"q": "caloi%20estradacle"}
+    # response = sessao.get(OLX_URL, headers=headers)
+    # print(response.request.headers)
+    # soup = BeautifulSoup(response.text, "html.parser")
+    # resu = soup.find(id="__NEXT_DATA__")
+    # with open("resu.html", "w", encoding="utf-8") as f:
+    #     f.write(soup.prettify())
+    # data = json.loads(getattr(resu, "text"))
+    # with open("resu.json", "w") as f:
+    #     json.dump(data, f, ensure_ascii=False, indent=4)
+    print(get_ad_data("caloi strada", stare_key="AL"))
