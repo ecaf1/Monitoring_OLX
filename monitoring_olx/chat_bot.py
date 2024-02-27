a052496ad6afe0c4b91dfdf5cf080156d7c48e7e
ecaf1 <edeilsonazevedo22@gmail.com>
2024-02-26 21:35:38 -0300

    Adiciona sesion e stados do bot

--- monitoring_olx/chat_bot.py
+++ monitoring_olx/chat_bot.py
@@ -1,32 +1,35 @@
 import os
 import time
-from functools import update_wrapper
 
 import requests
 from dotenv import load_dotenv
+
+from monitoring_olx import get_ad_data  # type: ignore
 
 load_dotenv()
 
 # Acessa as variáveis de ambiente
 TOKEN = os.getenv("TELEGRAM_TOKEN", "")
 URL = f"https://api.telegram.org/bot{TOKEN}/"
+state_chat = {}
+sesion = requests.Session()
+
+AWAITING_COMMAND, AWAITING_PRODUCT, AWAITING_STATE = range(3)
 
 
 def get_updates(last_update_id=None):
     url = URL + "getUpdates?timeout=100"
     if last_update_id:
         url += f"&offset={last_update_id}"
-    response = requests.get(url)
+    response = sesion.get(url)
     return response.json()
 
 
 def send_message(chat_id, text):
     url = URL + f"sendMessage?chat_id={chat_id}&text={text}"
 
-    response = requests.get(url)
+    response = sesion.get(url)
     x = response.json()
-    print(x)
-    return x
 
 
 def main():
@@ -39,7 +42,20 @@ def main():
                 update_id = update["update_id"]
                 chat_id = update["message"]["chat"]["id"]
                 mensage = update["message"]["text"]
-                print(chat_id)
+
+                current_state = state_chat.get(chat_id, AWAITING_COMMAND)
+
+                if current_state == AWAITING_COMMAND and mensage.lower() == "/olx":
+                    state_chat[chat_id] = AWAITING_PRODUCT
+                    send_message(chat_id, "envie o nome do produto")
+                elif current_state == AWAITING_PRODUCT:
+                    state_chat[chat_id] = AWAITING_STATE
+                    current_state.get(chat_id)
+                    send_message(chat_id, "Agora, envie a sigla do estado ou digite 'todo' para todo o Brasil.")
+                elif current_state == AWAITING_STATE:
+                    estado = mensage if mensage.lower() != 'todo' else None
+                    state_chat[chat_id] = AWAITING_COMMAND
+                    data_ad = get_ad_data()
             except Exception as e:
                 print(e)
             send_message(chat_id, "receba")
